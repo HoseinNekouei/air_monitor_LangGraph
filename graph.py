bf092e4c9ab290c8cc11fc9791f38b5989861ab2
Hossein Nekouei <nekouei.hosein@gmail.com>
2025-11-15 21:21:08 +0330

    added start tag

--- graph.py
+++ graph.py
@@ -1,4 +1,4 @@
-from langgraph.graph import StateGraph, END
+from langgraph.graph import StateGraph, START, END
 from state import State
 from nodes import (
     fetch_data_node, 
@@ -18,13 +18,11 @@ graph.add_node('logger', logger_node)
 
 # Flow:
 # fetch -> validate -> advice -> log -> END
+graph.add_edge(START, 'fetch')
 graph.add_edge('fetch', 'validate')
 graph.add_edge('validate', 'advice')
 graph.add_edge('advice', 'logger')
 graph.add_edge('logger', END)
-
-# set entry point
-graph.set_entry_point('fetch')
 
 app= graph.compile()
 
