1aa6ee4457e246583071a5d95fbc5182ff153cf4
Hossein Nekouei <nekouei.hosein@gmail.com>
2025-11-15 20:33:32 +0330

    rebuild Graph with new name node

--- graph.py
+++ graph.py
@@ -3,7 +3,7 @@ from state import State
 from nodes import (
     fetch_data_node, 
     validate_node, 
-    analyze_node,
+    advice_node,
     logger_node
     )
 
@@ -13,14 +13,14 @@ graph = StateGraph(State)
 
 graph.add_node('fetch',fetch_data_node)
 graph.add_node('validate', validate_node)
-graph.add_node('analyze', analyze_node)
+graph.add_node('advice', advice_node)
 graph.add_node('logger', logger_node)   
 
 # Flow:
-# fetch -> validate -> analyze -> log -> END
+# fetch -> validate -> advice -> log -> END
 graph.add_edge('fetch', 'validate')
-graph.add_edge('validate', 'analyze')
-graph.add_edge('analyze', 'logger')
+graph.add_edge('validate', 'advice')
+graph.add_edge('advice', 'logger')
 graph.add_edge('logger', END)
 
 # set entry point
