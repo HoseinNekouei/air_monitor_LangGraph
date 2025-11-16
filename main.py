f31f235e0b60e67277de52054edb6f1da92733e3
Hossein Nekouei <nekouei.hosein@gmail.com>
2025-11-16 11:49:37 +0330

    updated code for FastAPI

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import asyncio
-from graph import app
+from graph import app_graph
 from state import State
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
+
+app= FastAPI()
+
+@app.get('/')
 async def main():
     initial_state: State= {
         'raw': None,
@@ -10,15 +15,18 @@ async def main():
         'logs': []
     }
 
-    final_state= await app.ainvoke(initial_state)
+    final_state= await app_graph.ainvoke(initial_state)
 
-    # Access final state components
-    print("Final Advice:", final_state.get('advice'))
-    print("Logs:", final_state.get('logs'))
+    # # Access final state components
+    # print("Final Advice:", final_state.get('advice'))
+    # print("Logs:", final_state.get('logs'))
 
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+    return JSONResponse(
+            status_code=200, 
+            content= {
+                'Final Advice': final_state.get('advice'),
+                'Logs': final_state.get('logs') 
+                }
+             )
 
 
